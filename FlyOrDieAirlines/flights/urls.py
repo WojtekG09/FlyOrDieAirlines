fa8ebebc0f80c95876afdf2db49ab7b3f179b9e4
WojtekG09 <w.guzek09@gmail.com>
2023-04-02 17:56:03 +0200

    login/ registration

--- FlyOrDieAirlines/flights/urls.py
+++ FlyOrDieAirlines/flights/urls.py
@@ -6,8 +6,7 @@ from django.contrib.auth import views as auth_view
 app_name = "flights"
 
 urlpatterns = [
-  #  path("", views.homepage, name="homepage"),
     path("", views.home, name = 'home' ),
-    path("login/", auth_view.LoginView.as_view(template_name='flights/login.html'), name="login"),
+    path("login/", views.login_page, name="login"),
     path("register/", views.register_page, name="register"),
 ]
