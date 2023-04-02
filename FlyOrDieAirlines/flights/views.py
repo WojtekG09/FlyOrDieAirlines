0f0aa5d1e9ba463ac501c4726caafa5df8fa7c6f
KalinaPodyplomowe <pgx17581@student.wsb.gda.pl>
2023-04-02 21:48:17 +0200

    Search panel

--- FlyOrDieAirlines/flights/views.py
+++ FlyOrDieAirlines/flights/views.py
@@ -1,9 +1,12 @@
 from django.http import	HttpResponse
 from django.shortcuts import render
+from .models import Airport
 
-
+# def	home(request):
+# 	return render(request, 'flights/index.html')
 def	home(request):
-	return render(request, 'flights/index.html')
+    airports = Airport.objects.all()
+    return render(request, 'flights/index.html', {'airports': airports})
 
 def login_page(request):
 	return render(request, 'flights/login.html')
