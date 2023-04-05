67a650866a604116e7e5322c0fe00825cd524a67
WojtekG09 <w.guzek09@gmail.com>
2023-04-05 20:46:47 +0200

    login/logout + login required

--- FlyOrDieAirlines/flights/urls.py
+++ FlyOrDieAirlines/flights/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_view
-
+from django.contrib.auth import views as auth_views
 
 app_name = "flights"
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path("", views.home, name = 'home' ),
     path("login/", views.login_page, name="login"),
     path("register/", views.register_page, name="register"),
+    path("search/", views.search, name="search"),
+    path('logout/', views.logout_view, name='logout'),
+
 ]
