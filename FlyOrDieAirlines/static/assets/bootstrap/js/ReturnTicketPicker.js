5d82eb73ca1f2c309633749dd3213103b02b68b5
wg <w.guzek09@gmail.com>
2023-05-16 22:41:37 +0200

    ticket picking JS

---
+++ FlyOrDieAirlines/static/assets/bootstrap/js/ReturnTicketPicker.js
@@ -0,0 +1,14 @@
+function ReturnTicketPicker(button) {
+  var flightId = button.id.replace("istheflight_btn_return_", "");
+  var parentDiv = button.closest(".container_flight_return");
+  var divs = document.querySelectorAll('.container_flight_return');
+  divs.forEach(function(div) {
+    if (div !== parentDiv) {
+      if (div.style.display === "none") {
+        div.style.display = "block";
+      } else {
+        div.style.display = "none";
+      }
+    }
+  });
+}
