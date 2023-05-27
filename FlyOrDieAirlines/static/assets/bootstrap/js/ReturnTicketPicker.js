31b0f0ff21e86555d3d185f43cd86ac144760ca3
KalinaPodyplomowe <pgx17581@student.wsb.gda.pl>
2023-05-27 14:24:44 +0200

    quick fix

--- FlyOrDieAirlines/static/assets/bootstrap/js/ReturnTicketPicker.js
+++ FlyOrDieAirlines/static/assets/bootstrap/js/ReturnTicketPicker.js
@@ -1,22 +1,26 @@
 function ReturnTicketPicker(button)
 {
-  var flightId = button.id.replace("istheflight_btn_return_", "");
-  var parentDiv = button.closest(".container_flight_return");
-  var divs = document.querySelectorAll('.container_flight_return');
-  divs.forEach(function(div)
-  {
-    if (div !== parentDiv)
+    var flightId = button.id.replace("istheflight_btn_return_", "");
+    var parentDiv = button.closest(".container_flight_return");
+    var divs = document.querySelectorAll('.container_flight_return');
+    divs.forEach(function(div)
     {
-      if (div.style.display === "none")
-      {
-        div.style.display = "block";
-        document.getElementById('selectedReturnFlightId').value = "";
-      }
-      else
-      {
-        div.style.display = "none";
-        document.getElementById('selectedReturnFlightId').value = flightId;
-      }
+        if (div !== parentDiv)
+    {
+        if (div.style.display === "none")
+        {
+          div.style.display = "block";
+          document.getElementById('selectedReturnFlightId').value = "";
+        }
+        else
+        {
+          div.style.display = "none";
+          document.getElementById('selectedReturnFlightId').value = flightId;
+        }
     }
-  });
+    else
+        {
+            document.getElementById('selectedReturnFlightId').value = flightId;
+        }
+    });
 }
